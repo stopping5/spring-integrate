bc8ff5a86e16cd674127423f8b161fadc4ab79e1
stopping <1064076070@qq.com>
2022-05-16 00:14:16 +0800

    'feat:redis'

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-integrate
 spring框架整合项目案例仓库
+# 1.Spring整合Redis
+- 项目名称：spring-redis
+- 文档地址：https://mibcn03u9r.feishu.cn/wiki/wikcntwvihFEXA1yBVhwdiqR99e
+- 功能点
+- 1. Redis序列化
---
+++ spring-redis/src/main/java/com/stopping/SpringRedisApplication.java
@@ -0,0 +1,14 @@
+package com.stopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Springboot 启动类
+ */
+@SpringBootApplication
+public class SpringRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisApplication.class,args);
+    }
+}
