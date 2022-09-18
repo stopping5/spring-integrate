a157271be517e9db98056fea2cb6fa8eee965691
stopping <duoping.xing@ideamake.cn>
2022-09-18 16:54:18 +0800

    feat:spel init

---
+++ spring-spel/src/test/java/SpringSpelTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Classname: SpringSpelTest
+ * @Date: 2022/9/15 11:32 上午
+ * @author: stopping
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringSpelTest {
+
+    @Test
+    public void spel(){
+        System.out.println("test");
+    }
+}
