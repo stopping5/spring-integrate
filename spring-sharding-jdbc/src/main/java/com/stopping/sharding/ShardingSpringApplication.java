7e262257e4123a26669810da12810428e468b83e
stopping <1064076070@qq.com>
2023-02-27 00:42:41 +0800

    feat:sharding

---
+++ spring-sharding-jdbc/src/main/java/com/stopping/sharding/ShardingSpringApplication.java
@@ -0,0 +1,11 @@
+package com.stopping.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSpringApplication.class,args);
+    }
+}
